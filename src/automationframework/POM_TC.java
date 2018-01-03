06716c0e90fe857874d56fe0afc52f1f54abbdd0
Radhakrishnan Balakrishnan <radha@globalmouth.com>
2018-01-03 09:56:40 +0100

    Added POM TC code linking login page and home page class

--- src/automationframework/POM_TC.java
+++ src/automationframework/POM_TC.java
@@ -1,16 +1,12 @@
 package automationframework;
 
+  	import java.util.concurrent.TimeUnit;
+  	import org.openqa.selenium.WebDriver;
+  	import org.openqa.selenium.chrome.ChromeDriver;
 
-    import java.util.concurrent.TimeUnit;
-
-    import org.openqa.selenium.WebDriver;
-
-    import org.openqa.selenium.firefox.FirefoxDriver;
-
-    // Import package pageObject.*
+  	// Import package pageObject.
 
     import pageObjects.Home_Page;
-
     import pageObjects.Login_Page;
 
 public class POM_TC {
@@ -18,26 +14,26 @@ public class POM_TC {
 	private static WebDriver driver = null;
 
 	public static void main(String[] args) {
-
-	driver = new FirefoxDriver();
+		
+		driver = new ChromeDriver();
 	
-	driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
-	driver.get("http://www.zittyonline.com");
+		driver.get("http://www.zittyonline.com");
 
 	// Use page Object library now
 
-	Home_Page.lnk_MyAccount(driver).click();
+		Home_Page.lnk_MyAccount(driver).click();
 
-	Login_Page.txtbx_UserName(driver).sendKeys("0764354255");
+		Login_Page.txtbx_UserName(driver).sendKeys("0764354255");
 
-	Login_Page.txtbx_Password(driver).sendKeys("1511");
+		Login_Page.txtbx_Password(driver).sendKeys("1511");
 
-	Login_Page.btn_LogIn(driver).click();
+		Login_Page.btn_LogIn(driver).click();
 
-	System.out.println(" Login Successfully, now it is the time to Log Off buddy.");
+		System.out.println(" Login Successfully, now it is the time to Log Off buddy.");
 
-	Home_Page.lnk_LogOut(driver).click(); 
+		Home_Page.lnk_LogOut(driver).click(); 
 
 	driver.quit();
 
